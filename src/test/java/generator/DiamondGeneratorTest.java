a891efebda868b7d79a0e075e1680d22813a37a3
chen zhihao <18328576554@163.com>
2016-06-13 14:58:41 +0800

    add diamond generator and test and delete some empty test

---
+++ src/test/java/generator/DiamondGeneratorTest.java
@@ -0,0 +1,36 @@
+package generator;
+
+import org.junit.Before;
+import org.junit.Test;
+import printer.Printer;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+public class DiamondGeneratorTest {
+    private Printer printer;
+    private DiamondGenerator generator;
+
+    @Before
+    public void setUp() {
+        printer = mock(Printer.class);
+        generator = new DiamondGenerator(printer);
+    }
+
+    @Test
+    public void should_print_nothing_n_is_0() {
+        int n=0;
+        generator.generate(n);
+        verify(printer).print("");
+
+    }
+    @Test
+    public void should_print_correct_string_n_is_30() {
+        int n=3;
+        generator.generate(n);
+        verify(printer).print("  *\n ***\n*****\n ***\n  *\n");
+
+    }
+
+}
